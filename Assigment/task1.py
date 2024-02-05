4c78d49c3d2b5ed3193d20875183bb0fa2ff6b44
chhaya2906 <chhayap528@gmail.com>
2024-02-05 20:11:52 +0530

    1

--- Assigment/task1.py
+++
@@ -1,7 +0,0 @@
-num = int(input("enter the number : "))
-if num > 0:
-    print("The number is positive")
-elif num < 0:
-    print("The number is nagative")
-else:
-    print("The number is zero")    
--- Assigment/task2.py
+++
@@ -1,8 +0,0 @@
-n = int(input("enter number :  "))
-fac = 1
-
-for i in range(1,n+1):
-    fac = fac*i
-
-
-    print("factorial number : ",fac)
